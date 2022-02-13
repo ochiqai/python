714f47a0a65b8469e4fd684202b819f8522be1d7
ochiqai <ochiqaikontakt@gmail.com>
2022-02-13 15:49:48 +0000

    2-usul kursni olish ochun

--- kunlar/mini-proyektlar/kurs_konvertatsiya/konvert_dollar.py
+++ kunlar/mini-proyektlar/kurs_konvertatsiya/konvert_dollar.py
@@ -2,6 +2,7 @@
 import urllib.request, urllib.parse, urllib.error
 import ssl
 import gradio as gr
+import re
 
 
 def dollar_kursi():
@@ -15,8 +16,15 @@ def dollar_kursi():
     html_och = html.read()
     html_string = html_och.decode()
 
-    index = html_string.find("<strong>USD</strong><span>") #
-    return float(html_string[index + len("<strong>USD</strong><span>"):index + len("<strong>USD</strong><span>") + 8])
+    # 1 usul
+    # index = html_string.find("<strong>USD</strong><span>") #
+    # kurs = float(html_string[index + len("<strong>USD</strong><span>"):index + len("<strong>USD</strong><span>") + 8])
+
+    # 2-usul
+    lst = re.findall("USD</strong><span>([0-9.]+)", html_string)
+    kurs = lst[0]
+    kurs = float(kurs)
+    return kurs
 
 
 def konvert(dollar):
@@ -25,9 +33,9 @@ def konvert(dollar):
 
     return _dollar_kursi, jami
 
-# ulug = 50
-# _, som = konvert(50)
-# print(som)
+ulug = 50
+_, som = konvert(50)
+print(som)
 
-iface = gr.Interface(fn=konvert, inputs=["number"], outputs=["number", "number"])
-iface.launch(share=True)
+# iface = gr.Interface(fn=konvert, inputs=["number"], outputs=["number", "number"])
+# iface.launch(share=True)
