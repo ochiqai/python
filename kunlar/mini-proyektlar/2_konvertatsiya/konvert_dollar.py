55cfdc6cb5282ceb50a424be1671c9a135f70d21
ochiqai <ochiqaikontakt@gmail.com>
2022-02-20 15:54:33 +0000

    2-konvertatsiya, dollar->sum

--- kunlar/mini-proyektlar/2_konvertatsiya/konvert_dollar.py
+++ kunlar/mini-proyektlar/2_konvertatsiya/konvert_dollar.py
@@ -1,4 +1,3 @@
-# Search for link values within URL input
 import urllib.request, urllib.parse, urllib.error
 import ssl
 import gradio as gr
@@ -6,36 +5,56 @@ import re
 
 
 def dollar_kursi():
-    # Ignore SSL certificate errors
+    """
+    Ushbu funksiya dollar kursini https://daryo.uz/ saytga borib oladi va uni qaytaradi
+    :return: kursni qaytaradi, turi haqiqiy son
+    """
+    # ushbu 3 qator, hozircha muhim emas. Ular netga bog'langanda
+    # shunchaki malumot beradi
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
 
+    # web address (url)
     url = "https://daryo.uz/"
+    # web addressni ochish
     html = urllib.request.urlopen(url, context=ctx)
+    # Ochilgan web adressni o'qish, odatdan ikkilik (binary) ko'rinishda bo'ladi
     html_och = html.read()
+    # o'qilganni ikkilikdan stringga o'tkazish
     html_string = html_och.decode()
 
-    # 1 usul
+    # berilgan stringdan dollar kurisni topish
+    # 1 usul: string funksiyalaridan foydalanib qilish
     # index = html_string.find("<strong>USD</strong><span>") #
     # kurs = float(html_string[index + len("<strong>USD</strong><span>"):index + len("<strong>USD</strong><span>") + 8])
 
-    # 2-usul
+    # 2-usul: regular ifodalardan foydalanib qilish
     lst = re.findall("USD</strong><span>([0-9.]+)", html_string)
     kurs = lst[0]
-    kurs = float(kurs)
-    return kurs
+    return float(kurs)
 
 
 def konvert(dollar):
+    """
+    Foydalanivchidan dollarni oladi, va uni so'mga o'tkazadi
+    :param dollar: miqdor foydalanichni tomonidan. Masalan, 60
+    :return: 2 ta qaytaradi: 1) kurs 2) jami
+    """
+
+    # joriy kursni olish
     _dollar_kursi = dollar_kursi()
+
+    # joriy kurs bo'yicha, so'mga o'tkazish
     jami = dollar * _dollar_kursi
 
     return _dollar_kursi, jami
 
+# Qora ekranda ishlatish
 ulug = 50
 _, som = konvert(50)
 print(som)
 
-# iface = gr.Interface(fn=konvert, inputs=["number"], outputs=["number", "number"])
-# iface.launch(share=True)
+# Gradioda ishlatish
+iface = gr.Interface(fn=konvert, inputs=["number"], outputs=["number", "number"])
+iface.launch(share=True)
