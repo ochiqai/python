9c9e64ee91138bbe092a7753bd9a213ad1e9ae9e
NuriddinAI <nuriddinturobov595@gmail.com>
2023-01-08 21:56:43 +0500

    kodlar uzgardi

--- projects/gradio_apps/prezident_aniqlash.py
+++ projects/gradio_apps/prezident_aniqlash.py
@@ -1,5 +1,6 @@
 import gradio as gr
 import cv2
+import math
 ASLIDA = [
     "Bu O'zbekiston prezidenti Shavkat Mirziyoyev",
     "Bu Rassiya prezidenti Vladimir Putin",
@@ -10,7 +11,10 @@ from projects.gradio_apps.refaktor.yuzbib import (
     model_yuklash, aniqlash, vector_taq
 )
 
-def dastur(baza_rasm_1, baza_rasm_2, baza_rasm_3, test_rasm):
+baza_rasm_1 = cv2.imread("./prezident_image_baza/mirziyoyev.jpg")
+baza_rasm_2 = cv2.imread("./prezident_image_baza/putin.jpg")
+baza_rasm_3 = cv2.imread("./prezident_image_baza/bayden.jpg")
+def dastur(test_rasm):
 
     aniqlagich_model = model_yuklash(turi="aniqlagich")
     embedding_model = model_yuklash(turi="embedding")
@@ -39,23 +43,23 @@ def dastur(baza_rasm_1, baza_rasm_2, baza_rasm_3, test_rasm):
     test_embedding = embedding_model.get(test_rasm, kordinatalar_format_test[0])
 
     baza_list = [baza_embedding_1, baza_embedding_2, baza_embedding_3]
-
+    yaqinliklar = []
     for idx in range(0, len(baza_list)):
         yaqinlik = vector_taq(test_embedding.tolist(), baza_list[idx].tolist())
-        if yaqinlik > 0.1:
-            return ASLIDA[idx]
-    else:
-        return "Bu inson prezident emas"
+        yaqinliklar.append(yaqinlik)
+        # 0.007, 0.12, 0.4
+    max_yaqinlik = max(yaqinliklar) # 0.4
+    for i in range(len(yaqinliklar)):
+        if yaqinliklar[i] == max_yaqinlik: # i = 2
+            return ASLIDA[i], yaqinlik
+    return "Bu inson prezident emas"
 
 # gradio
 with gr.Blocks() as demo:
-    name1 = gr.Image(label="Baza rasm 1")
-    name2 = gr.Image(label="Baza rasm 2")
-    name3 = gr.Image(label="Baza rasm 3")
-    name4 = gr.Image(label="Test rasm")
+    name= gr.Image(label="Test rasm")
     output_text = gr.Textbox(label="Malumot")
     tugma = gr.Button("Bosing")
-    tugma.click(fn=dastur, inputs=[name1, name2, name3, name4], outputs=output_text)
+    tugma.click(fn=dastur, inputs=name, outputs=output_text)
 
 demo.launch(share=True)
 
