c398503dac973c6272a2df8005f6623a3a23a2bb
NuriddinAI <nuriddinturobov595@gmail.com>
2023-01-15 15:24:48 +0500

    -

--- projects/gradio_apps/prezident_aniqlash.py
+++ projects/gradio_apps/prezident_aniqlash.py
@@ -11,26 +11,42 @@ from projects.gradio_apps.refaktor.yuzbib import (
     model_yuklash, aniqlash, vector_taq
 )
 
+def kordinata_format(model, rasm):
+    """
+        Funksiya rasmning kordinatasini qaytaradi
+    :param model:
+         aniqlagich model
+    :param rasm:
+        rasm
+    :return:
+        rasm kordinatasi
+    """
+    kordinata = aniqlash(
+        model=model,
+        rasm=rasm
+    )
+    return kordinata[0]
+
 baza_rasm_1 = cv2.imread("./prezident_image_baza/mirziyoyev.jpg")
 baza_rasm_2 = cv2.imread("./prezident_image_baza/putin.jpg")
 baza_rasm_3 = cv2.imread("./prezident_image_baza/bayden.jpg")
+
 def dastur(test_rasm):
+    # modellarni yuklash
     aniqlagich_model = model_yuklash(turi="aniqlagich")
     embedding_model = model_yuklash(turi="embedding")
+    # bazadagi rasmlar
     baza_rasmlar = [baza_rasm_1, baza_rasm_2, baza_rasm_3]
+    # bazadagi rasmlarning embeddingni olish
     baza_embeddinglar = []
     for baza_rasm in baza_rasmlar:
-        kordinatalar_format = aniqlash(
-            model=aniqlagich_model,
-            rasm=baza_rasm
-        )
-        baza_embedding = embedding_model.get(baza_rasm, kordinatalar_format[0])
+        baza_rasm_kordinata = kordinata_format(aniqlagich_model, baza_rasm)
+        baza_embedding = embedding_model.get(baza_rasm, baza_rasm_kordinata)
         baza_embeddinglar.append(baza_embedding)
-    kordinatalar_format_test = aniqlash(
-        model=aniqlagich_model,
-        rasm=test_rasm
-    )
-    test_embedding = embedding_model.get(test_rasm, kordinatalar_format_test[0])
+    # test rasm embedding olish
+    test_rasm_kordinata = kordinata_format(aniqlagich_model, test_rasm)
+    test_embedding = embedding_model.get(test_rasm, test_rasm_kordinata)
+
     yaqinliklar = []
     for idx in range(0, len(baza_embeddinglar)):
         yaqinlik = vector_taq(test_embedding.tolist(), baza_embeddinglar[idx].tolist())
