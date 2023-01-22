e208e489fa145ad6c2f3bb95550f67eb3e09798c
NuriddinAI <nuriddinturobov595@gmail.com>
2023-01-22 22:29:18 +0500

    Kamera orqali yuz taqqoslash

--- projects/gradio_apps/kameradan_yuz_aniqlash.py
+++ projects/gradio_apps/kameradan_yuz_aniqlash.py
@@ -1,26 +1,32 @@
+import cv2
 import gradio as gr
 
 from projects.gradio_apps.refaktor.yuzbib import (
-    model_yuklash, aniqlash, chizish
+    model_yuklash, aniqlash, vector_taq
 )
 
+baza_rasm = cv2.imread("./prezident_image_baza/nuriddin.jpg")
+
 def kamera(rasm):
-    # rasm: class 'numpy.ndarray'
     aniqlagich_model = model_yuklash(turi="aniqlagich")
-    kordinatalar = aniqlash(
+    embedding_modul = model_yuklash(turi="embedding")
+    rasm_kordinatalar = aniqlash(
         model=aniqlagich_model,
         rasm=rasm
     )
-    rasm_chizilgan = chizish(
-        rasm=rasm,
-        kordinatalar=kordinatalar
-    )
-    return rasm_chizilgan
+    baza_rasm_kordinata = aniqlash(aniqlagich_model, baza_rasm)
+
+    rasm_embedding = embedding_modul.get(rasm, rasm_kordinatalar)
+    baza_rasm_embedding = embedding_modul.get(baza_rasm, baza_rasm_kordinata)
+
+    yaqinlik = vector_taq(rasm_embedding.tolist(), baza_rasm_embedding.tolist())
+
+    return yaqinlik
 
 demo = gr.Interface(
     kamera,
     gr.Image(source="webcam", streaming=True),
-    "image",
+    "text",
     live=True
 )
 
