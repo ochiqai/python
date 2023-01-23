c3278aaf87a645a717d1da7052713e813e3b4479
NuriddinAI <nuriddinturobov595@gmail.com>
2023-01-23 21:53:44 +0500

    doc type yozildi

--- projects/gradio_apps/kameradan_yuz_aniqlash.py
+++ projects/gradio_apps/kameradan_yuz_aniqlash.py
@@ -1,11 +1,34 @@
 import gradio as gr
 
+
 from projects.gradio_apps.refaktor.yuzbib import (
     model_yuklash, aniqlash, chizish
 )
 
+#------------- Dastur haqida ----------------------
+#  Kameradan yuz qismini aniqlash
+#--------------------------------------------------
+
+#------------- Algoritm ---------------------------
+#   1. Kerakli modullarni yuklash. (detection)
+#   2. Kordinata aniqlanadi.
+#   3. rasm_chizilgan funksiyasini yuklash.
+#--------------------------------------------------
+
+
+
 def kamera(rasm):
-    # rasm: class 'numpy.ndarray'
+    """
+    Kamera orqali kiritilgan rasmni yuz qismini aniqlaydi.
+    Parameters
+    ----------
+    rasm: numpy
+
+    Returns
+    -------
+    rasm : numoy
+        chizilgan rasm
+    """
     aniqlagich_model = model_yuklash(turi="aniqlagich")
     kordinatalar = aniqlash(
         model=aniqlagich_model,
